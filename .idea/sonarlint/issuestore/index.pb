6f5050a3b5bb3c8a4547972c5847386f0ced4dc2
Arisusantolie <arisusantolie12@gmail.com>
2023-10-21 19:01:43 +0700

    add InvalidaSessionFilter

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -3,13 +3,5 @@ h
 8src/main/java/com/training/keycloak/sso/Application.java,c/2/c2da15624697a0b4827654f60200cd4b10a0b3e4
 9
 	README.md,8/e/8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
-t
-Dsrc/main/java/com/training/keycloak/sso/security/SecurityConfig.java,d/7/d710b074c6e95c48a1de7725265b2affaf76d3f8
-u
-Esrc/main/java/com/training/keycloak/sso/security/SecurityConfig2.java,b/6/b61fef68d65e79fadb5b7f39cb04f281df25b39e
-r
-Bsrc/main/java/com/training/keycloak/sso/security/WebSecurity2.java,9/1/91278eb38b4b3fc9217956da57fd59bc56644ed2
-q
-Asrc/main/java/com/training/keycloak/sso/security/WebSecurity.java,a/d/ad927eaaa54aef1731711120bdcb1a118f67a959
 7
 pom.xml,4/4/442292b8a7efeabbe4cc176709b833b1792140ec
---
+++ src/main/java/com/training/keycloak/sso/security/InvalidateSessionFilter.java
@@ -0,0 +1,82 @@
+package com.training.keycloak.sso.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.*;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class InvalidateSessionFilter extends OncePerRequestFilter {
+
+    @Autowired
+    OAuth2AuthorizedClientService clientService;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Autowired
+    KeycloakLogoutHandler keycloakLogoutHandler;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        System.out.println(request.getRequestURI());
+        Authentication authentication =
+                SecurityContextHolder
+                        .getContext()
+                        .getAuthentication();
+
+        try {
+            // Check the user's session on the Keycloak server
+            //auth/realms/develop/protocol/openid-connect/userinfo
+//            System.out.println(authentication.getName()+"   aro");
+            OAuth2AuthenticationToken oauthToken = (OAuth2AuthenticationToken) authentication;
+
+            OAuth2AuthorizedClient client =
+                    clientService.loadAuthorizedClient(
+                            oauthToken.getAuthorizedClientRegistrationId(),
+                            oauthToken.getName());
+
+            String accessToken = client.getAccessToken().getTokenValue();
+
+//            System.out.println(accessToken);
+            String sessionValidationUrl = "http://localhost:8080/realms/training/protocol/openid-connect/userinfo";
+
+            HttpHeaders headers = new HttpHeaders();
+//            System.out.println("oidcUser.getUserInfo().toString() = "+oidcUser.getUserInfo().toString().toString());
+            headers.set("Authorization", "Bearer "+accessToken);
+            HttpEntity<?> entity = new HttpEntity<>(headers);
+
+//            restTemplate.getForEntity(sessionValidationUrl, HttpMethod.POST,entity, Void.class);
+            ResponseEntity<Void> userInfo = restTemplate.exchange(sessionValidationUrl, HttpMethod.GET, entity, Void.class);
+//            System.out.println(userInfo.getStatusCode());
+            // If the session is valid, continue with the login
+            filterChain.doFilter(request, response);
+
+
+        } catch (HttpClientErrorException e) {
+//            e.printStackTrace();
+            if (e.getStatusCode() == HttpStatus.UNAUTHORIZED) {
+                // If the session is no longer valid, you can log the user out or take other appropriate actions.
+                request.getSession().invalidate();
+                // Redirect to a logout success page or other appropriate action
+                response.sendRedirect("/");
+            } else {
+                // Handle other errors as needed
+//                filterChain.doFilter(request, response);
+            }
+        }
+    }
+}
